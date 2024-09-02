b1f9bbe3c1e71ac5322f7f5e294d6a2f0c8b536e
Paulo Cesar <paulo.cesar@vitaebrasil.com.br>
2024-09-02 19:54:22 -0300

    include functions on buttons

--- src/app/components/modal-users/modal-users.component.ts
+++ src/app/components/modal-users/modal-users.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {
+  MatDialog,
   MatDialogActions,
   MatDialogClose,
   MatDialogContent,
@@ -8,6 +9,8 @@ import {
 import {MatListModule} from '@angular/material/list';
 import {MatIconModule} from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { ModalRegisterUserComponent } from '../modal-register-user/modal-register-user.component';
+import { ModalConfirmComponent } from '../modal-confirm/modal-confirm.component';
 
 
 @Component({
@@ -26,5 +29,29 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrl: './modal-users.component.scss'
 })
 export class ModalUsersComponent {
+  constructor(public dialog: MatDialog)
+  { }
 
+  public registerNewUserModal(id: string) {
+    this.dialog.closeAll();
+    this.dialog.open(ModalRegisterUserComponent, {
+      minWidth: '600px',
+      data: {id}
+    });
+  }
+
+  public deleteUser(id: string): void {
+    // After implements request or similar to delete user, and verify if selected user if admin to possibility delete.
+    // in poker, this is like a note 5;
+    console.log('deleteUser');
+    this.dialog.closeAll();
+    const dialogRef = this.dialog.open(ModalConfirmComponent, {
+      minWidth: '300px'
+    })
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      console.log(result);
+    });
+  }
 }
