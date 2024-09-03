e727f63845e7f75846dc34f9776a7695eda4754b
Paulo Cesar <paulo.cesar@vitaebrasil.com.br>
2024-09-03 17:39:01 -0300

    import new component to list tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { CoreModule } from './core/core.module';
+import { ListContainerComponent } from "./components/list-container/list-container.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, CoreModule],
+  imports: [RouterOutlet, HeaderComponent, CoreModule, ListContainerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
