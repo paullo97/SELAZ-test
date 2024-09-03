9036db6d367a97268a92167be77955efc326c024
Paulo Cesar <paulo.cesar@vitaebrasil.com.br>
2024-09-03 15:18:22 -0300

    include system to badge in button and selected user

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
@@ -6,6 +6,8 @@ import {
   MatDialog,
 } from '@angular/material/dialog';
 import { ModalUsersComponent } from '../modal-users/modal-users.component';
+import { LocalStorageService } from '../../core/services/local-storage.service';
+import {MatBadgeModule} from '@angular/material/badge';
 
 @Component({
   selector: 'app-header',
@@ -13,18 +15,43 @@ import { ModalUsersComponent } from '../modal-users/modal-users.component';
   imports: [
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatBadgeModule
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
-  constructor(public dialog: MatDialog)
+export class HeaderComponent implements OnInit, OnChanges{
+  public selectUser: any = {};
+  public listUsersLength = 0;
+
+  constructor(
+    public dialog: MatDialog,
+    private localStorage: LocalStorageService<any>
+  )
   { }
 
+  public ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+  }
+
+  public ngOnInit(): void {
+    this.listUsersLength = this.localStorage.getItem('listUsers').length;
+  }
+
   public openDialogUser() {
-    this.dialog.open(ModalUsersComponent, {
+    const dialogUsers = this.dialog.open(ModalUsersComponent, {
       minWidth: '600px',
     });
+
+    dialogUsers.afterClosed().subscribe((result) => {
+      if(result) {
+        this.localStorage.setItem('selectUser', result);
+        this.selectUser = result;
+      }
+
+      console.log(this.localStorage.getItem('listUsers').length);
+      this.listUsersLength = this.localStorage.getItem('listUsers').length;
+    })
   }
 }
