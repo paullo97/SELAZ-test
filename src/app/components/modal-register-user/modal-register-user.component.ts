a465a987d47a2c8a245296fe2e4c83d53c9ceaca
Paulo Cesar <paulo.cesar@vitaebrasil.com.br>
2024-09-03 15:19:42 -0300

    Fix methods to register and edit user

--- src/app/components/modal-register-user/modal-register-user.component.ts
+++ src/app/components/modal-register-user/modal-register-user.component.ts
@@ -9,6 +9,8 @@ import {
   MatDialogRef,
 } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
+import { LocalStorageService } from '../../core/services/local-storage.service';
+import { UuidService } from '../../core/services/uuid.service';
 @Component({
   selector: 'app-modal-register-user',
   standalone: true,
@@ -24,20 +26,39 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrl: './modal-register-user.component.scss'
 })
 export class ModalRegisterUserComponent implements OnInit {
+  constructor(
+    private localStorage: LocalStorageService<any>,
+    private uuid: UuidService
+  )
+  { }
+
   public ngOnInit(): void {
     if(this.data.id.length > 0) {
       // After implements request o localStorage or similar
-      console.log(this.data);
+      // FIX after when this works
+      const { name, role, id } = this.localStorage.getItem('listUsers').filter((user: any) => user.id === this.data.id)[0];
+      this.name = name;
+      this.role = role;
+      this.id = id;
     }
   }
+
   readonly dialogRef = inject(MatDialogRef<ModalRegisterUserComponent>);
   readonly data = inject<{ id: string, name: string, role: string}>(MAT_DIALOG_DATA);
 
   public name: string = '';
   public role: string = '';
+  protected id: string = '';
 
   public register(): void {
-    console.log(this.name, this.role);
+    this.dialogRef.close({
+      id: this.id || this.uuid.generateUUID(),
+      name: this.name,
+      role: this.role
+    });
+    this.name = '';
+    this.role = '';
+    this.id = '';
   }
 
   public onNoClick(): void {
