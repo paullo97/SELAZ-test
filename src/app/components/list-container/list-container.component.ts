53a3c3c0ddf4597a3e365f69183e1254d567a798
Paulo Cesar <paulo.cesar@vitaebrasil.com.br>
2024-09-03 23:26:22 -0300

    WIP

--- src/app/components/list-container/list-container.component.ts
+++ src/app/components/list-container/list-container.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { LocalStorageService } from '../../core/services/local-storage.service';
+import { ToolsHeaderListComponent } from '../tools-header-list/tools-header-list.component';
 
 @Component({
   selector: 'app-list-container',
@@ -12,22 +13,14 @@ import { LocalStorageService } from '../../core/services/local-storage.service';
     MatCardModule,
     MatListModule,
     MatIconModule,
-    MatTooltipModule
+    MatTooltipModule,
+    ToolsHeaderListComponent
   ],
   templateUrl: './list-container.component.html',
   styleUrl: './list-container.component.scss'
 })
 export class ListContainerComponent implements OnInit {
    public task: Array<any> = [
-    {
-      id: '',
-      titulo: 'teste1',
-      descricao: '',
-      dataCriacao: '',
-      dataVencimento: '',
-      status: 0,
-      responsavel: 'Admin'
-    },
     {
       id: '',
       titulo: 'teste2',
@@ -62,7 +55,7 @@ export class ListContainerComponent implements OnInit {
    { }
 
   public ngOnInit(): void {
-    // this.task = this.localStorage.getItem('listTask');
+    this.task = this.localStorage.getItem('listTask');
     this.selectUser = this.localStorage.getItem('selectUser');
   }
 
@@ -92,7 +85,8 @@ export class ListContainerComponent implements OnInit {
    public handleDeleteTask(idTask: string): void {
     console.log('handleDeleteTask', idTask);
 
-    if(this.selectUser.role !== 'admin') { // Change to ENUM
+    console.log(this.selectUser);
+    if(this.selectUser.role !== "admin") { // Change to ENUM
       console.log('Não é possivel deletar sem ser o Admin');
       return;
     }
@@ -100,7 +94,9 @@ export class ListContainerComponent implements OnInit {
     this.localStorage.removeItemArray('listTask', idTask);
    }
 
-   public handleRegisterTask() {
+   public handleRegisterTask(newTasks: any) {
     // TODO: Implements method to register new tasks
+    this.localStorage.addItemToArray('listTask', newTasks); // FIX Later, change to Enum
+    this.task = this.localStorage.getItem('listTask');
    }
 }
