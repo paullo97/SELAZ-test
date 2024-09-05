e1f5acdf65529d38f1e5592e8e10d0241dd6f1c1
Paulo Cesar <paulo.cesar@vitaebrasil.com.br>
2024-09-05 10:37:10 -0300

    create componente to align some inputs in a row

---
+++ src/app/components/form/row/row.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'Row',
+  standalone: true,
+  imports: [],
+  templateUrl: './row.component.html',
+  styleUrl: './row.component.scss'
+})
+export class RowComponent {
+
+}
