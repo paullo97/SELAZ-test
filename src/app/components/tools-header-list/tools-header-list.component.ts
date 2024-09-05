72219b7702a98ba57d76a99758c31fd7280ddda1
Paulo Cesar <paulo.cesar@vitaebrasil.com.br>
2024-09-04 21:54:48 -0300

    Fix imports

--- src/app/components/tools-header-list/tools-header-list.component.ts
+++ src/app/components/tools-header-list/tools-header-list.component.ts
@@ -1,19 +1,32 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { MatButton } from '@angular/material/button';
-import { MatDialog } from '@angular/material/dialog';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
 import { ModalCreateTaskComponent } from '../modals/modal-create-task/modal-create-task.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatOptionModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-tools-header-list',
   standalone: true,
   imports: [
-    MatButton,
-    MatFormFieldModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatBadgeModule,
+    MatToolbarModule,
+    MatIconModule,
+    CommonModule,
     MatSelectModule,
-    MatInputModule
+    MatFormFieldModule,
+    MatOptionModule
   ],
   templateUrl: './tools-header-list.component.html',
   styleUrl: './tools-header-list.component.scss'
@@ -32,8 +45,8 @@ export class ToolsHeaderListComponent {
 
   public showModalRegisterTask() {
     const dialogRegisterTask = this.dialog.open(ModalCreateTaskComponent, {
-      minHeight: '60vh'
-    })
+      minHeight: '60vh',
+    });
 
     dialogRegisterTask.afterClosed().subscribe((result: any) => {
       if(result) {
